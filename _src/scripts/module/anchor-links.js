f9a4f4372e69361a2b44b916a7d8b2657719cc96
Fintisov Vadim <fintisov.vadim@gmail.com>
2022-09-19 13:59:34 +0300

    added anchor links

---
+++ _src/scripts/module/anchor-links.js
@@ -0,0 +1,8 @@
+import SmoothScroll from "smooth-scroll"
+
+const anchorLinks = new SmoothScroll('a[href*="#"]', {
+    speed: 300,
+
+});
+
+export default scroll;
--- _src/scripts/script.js
+++ _src/scripts/script.js
@@ -1,7 +1,9 @@
 import bannerSlider from "./module/main-banner__slider";
+import anchorLinks from "./module/anchor-links";
 import navMenu from "./module/nav-menu";
 
 window.addEventListener("DOMContentLoaded", ()=> {
    bannerSlider;
+   anchorLinks;
    navMenu();
 })
